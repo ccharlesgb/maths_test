607e1a8c3ecce84e1795fbc293ccf12fc1bd220a
Connor Charles <ccharles.gb@gmail.com>
2019-08-28 19:03:28 +0100

    Added current user route

--- maths_app/api/routes.py
+++ maths_app/api/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from maths_app import guard
+from flask_praetorian import auth_required, current_user
 
 api = Blueprint("api", __name__, url_prefix="/api")
 
@@ -14,3 +15,11 @@ def login():
     req_data = request.get_json(force=True)
     user = guard.authenticate(req_data.get("username"), req_data.get("password"))
     return jsonify({"token": guard.encode_jwt_token(user)})
+
+
+@api.route("/current_user", methods=["GET"])
+@auth_required
+def current_username():
+    cur_user = current_user()
+    resp = {"username": cur_user.username, "fullname": cur_user.full_name}
+    return resp, 200
--- maths_app/tests/test_api.py
+++ maths_app/tests/test_api.py
@@ -1,6 +1,15 @@
 import json
+from maths_app.models import User, guard
 
 
 def test_login(client):
     response = client.post("/api/login", data=json.dumps({"username": "admin", "password": "admin"}))
     assert "token" in response.json.keys()
+
+
+def test_current_user(client):
+    with client.application.app_context():
+        admin_user = User.lookup("admin")
+        admin_header = guard.pack_header_for_user(admin_user)
+    response = client.get("/api/current_user", headers=admin_header)
+    assert response.json["username"] == "admin"
