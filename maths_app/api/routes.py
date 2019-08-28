fb2e71c795bfebfda1d8de0a7bb48654c047de70
Connor Charles <ccharles.gb@gmail.com>
2019-08-28 19:17:53 +0100

    Added register route for students

--- maths_app/api/routes.py
+++ maths_app/api/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from maths_app import guard
+from maths_app import guard, utils, models
+from maths_app.models import db
 from flask_praetorian import auth_required, current_user
 
 api = Blueprint("api", __name__, url_prefix="/api")
@@ -23,3 +24,17 @@ def current_username():
     cur_user = current_user()
     resp = {"username": cur_user.username, "fullname": cur_user.full_name}
     return resp, 200
+
+
+@api.route("/register", methods=["POST"])
+def register_user():
+    req_data = request.get_json(force=True)
+    # Pop password hash out before we marshal this as we need to hash it
+    password = req_data.pop("password")
+    new_user = models.UserSchema().load(req_data)
+    new_user.password = guard.hash_password(password)
+    new_user.role = utils.get_role("student")  # Only students can register
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "New user created", "id": new_user.id}), 200
--- maths_app/tests/test_api.py
+++ maths_app/tests/test_api.py
@@ -13,3 +13,19 @@ def test_current_user(client):
         admin_header = guard.pack_header_for_user(admin_user)
     response = client.get("/api/current_user", headers=admin_header)
     assert response.json["username"] == "admin"
+
+
+def test_register_user(client):
+    user = "new_student"
+    password = "pass1"
+    user_data = {"username": user,
+                 "password": password,
+                 "email": "test@mail.com",
+                 "full_name": "New Student"}
+    response = client.post("/api/register", data=json.dumps(user_data))
+
+    assert "id" in response.json.keys()
+
+    # Test that we can now log in with these credentials
+    response = client.post("/api/login", data=json.dumps({"username": user, "password": password}))
+    assert "token" in response.json.keys()
