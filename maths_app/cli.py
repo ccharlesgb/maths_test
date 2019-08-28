a9c9ec3cd0a15f47ce13ded7d3169482fd80aefb
Connor Charles <ccharles.gb@gmail.com>
2019-08-28 22:14:58 +0100

    Added uwsgi config and cli for init_db
    uwsgi will enable us to run deploy to a production environment in nginx.
    The cli tool will allow us to run admin commands in prod as well

--- maths_app/cli.py
+++ maths_app/cli.py
@@ -1,8 +1,22 @@
 import click
 from flask.cli import FlaskGroup
 from . import create_app
+from .utils import init_db
+from maths_app import guard
 
 
 @click.group(cls=FlaskGroup, create_app=create_app)
 def cli():
     pass
+
+
+@cli.command()
+def initdb():
+    """
+    Initialise the database. Should only be run once to bootstrap the application
+
+    1. Creates the database tables
+    2. Creates the 3 roles defined in project spec
+    3. Creates the first user 'admin' to allow administration
+    """
+    init_db()
